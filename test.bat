99362bb724a737a8da02778b5c400b7dc4abd52c
borasaki <huioerty645@gmail.com>
2024-07-03 07:42:55 -0400

    Removed unused libraries and added win32. Outline for Macro installed.

--- test.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-SET "PScommand="POWERSHELL Add-Type -AssemblyName System.Windows.Forms; $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ValidateNames = $false;FileName='';}; $FileBrowser.ShowDialog(); $File = $FileBrowser.Filename; Write-Output $File;""
-
-FOR /F "Skip=1 usebackq tokens=*" %%Q in (`%PScommand%`) DO (
-    ECHO %%Q
-    del "config.txt"
-    ECHO %%Q>> config.txt
-)
